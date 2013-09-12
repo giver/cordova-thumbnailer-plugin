bebe0cbeda9b1a1d919c72672eec3a5cc0bc4784
Tanin Srivaraphong <tanin.s@outlook.com>
2013-09-13 00:48:37 +0700

    Update thumbnailer.js

--- thumbnailer.js
+++ thumbnailer.js
@@ -9,57 +9,31 @@ cordova.define("cordova/plugin/thumbnailer", function(require, exports, module) 
 
 	var Thumbnailer = function() {};
 
-	MyPlugin.prototype.foo = function(successCallback,failureCallback) {
-	    exec(successCallback, failureCallback, 'MyPlugin', 'foo', []);
+	Thumbnailer.prototype.createVideoThumbnail = function(srcFilePath, successCallback, failureCallback) {
+	    if ( srcFilePath.toLowerCase().indexOf( "file://" ) === 0 ) {
+            srcFilePath = srcFilePath.substring( 7 );
+        }
+	    
+	    exec(successCallback, failureCallback, 'Thumbnailer', 'createVideoThumbnail', [srcFilePath]);
 	}
 
-	MyPlugin.prototype.bar = function(successCallback,failureCallback) {
-	    exec(successCallback, failureCallback, 'MyPlugin', 'bar', []);
+	Thumbnailer.prototype.createImageThumbnail = function(srcFilePath, successCallback,failureCallback) {
+	    if ( srcFilePath.toLowerCase().indexOf( "file://" ) === 0 ) {
+            srcFilePath = srcFilePath.substring( 7 );
+        }
+	    
+	    exec(successCallback, failureCallback, 'Thumbnailer', 'createImageThumbnail', [srcFilePath]);
 	}
 
-	MyPlugin.prototype.baz = function(successCallback,failureCallback) {
-	    exec(successCallback, failureCallback, 'MyPlugin', 'baz', []);
-	}
-
-	var myplugin = new MyPlugin();
-	module.exports = myplugin;
+	module.exports = new Thumbnailer();
 });
-
-/**
- * Constructor
- */
-function Thumbnailer() {
-};
-
-/**
- * Starts the video player intent
- *
- * @param url           The url to play
- */
-Thumbnailer.prototype.createVideoThumbnail = function ( url, callback ) {
-    if ( url.toLowerCase().indexOf( "file://" ) === 0 ) {
-        url = url.substring( 7 );
-    }
-    cordova.exec( callback, thumbError, "Thumbnailer", "createVideoThumbnail", [url] );
-};
-Thumbnailer.prototype.createImageThumbnail = function ( url, callback ) {
-    if ( url.toLowerCase().indexOf( "file://" ) === 0 ) {
-        url = url.substring( 7 );
-    }
-    cordova.exec( callback, thumbError, "Thumbnailer", "createImageThumbnail", [url] );
-};
-
-function thumbError( err ) {
-    alert( 'Error creating thumbnail!' );
-};
 
 /**
  * Load Thumbnailer
  */
-
 if ( !window.plugins ) {
     window.plugins = {};
 }
 if ( !window.plugins.thumbnailer ) {
-    window.plugins.thumbnailer = new Thumbnailer();
+    window.plugins.thumbnailer = cordova.require("cordova/plugin/thumbnailer");
 }
